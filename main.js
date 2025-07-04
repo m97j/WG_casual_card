fb0f054d5c2fbe0ab655f0b8b040a08e36154b6a
m97j <mmnkjiae@gmail.com>
2025-07-05 00:48:22 +0900

    main page implements

---
+++ main.js
@@ -0,0 +1,7 @@
+function openGame(gameName) {
+  if (gameName === 'card-flip') {
+    window.location.href = './card-flip/index.html';
+  } else if (gameName === 'breakout') {
+    window.location.href = './breakout/index.html';
+  }
+}
